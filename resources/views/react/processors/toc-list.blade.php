a4a1f90ff76697b2d946e38686433d00a6c046c1
Robin Radic <robin@radic.nl>
2019-01-15 18:30:46 +0100

    upd

--- resources/views/react/processors/toc-list.blade.php
+++ resources/views/react/processors/toc-list.blade.php
@@ -1,11 +1,11 @@
 {{--<toc-list class="{{$list_class}}">--}}
-<ul class="c-toc-list">
+<c-toc-list>
     @foreach($items as $item)
-        <li class="c-toc-list-item">
-            <a href="#{{ $item->getValue()->getSlug()  }}" title="{{ $item->getValue()->getText() }}">{{ $item->getValue()->getText() }}</a>
+        <c-toc-list-item href="#{{ $item->getValue()->getSlug()  }}" title="{{ $item->getValue()->getText() }}">
+{{--            <a >{{ $item->getValue()->getText() }}</a>--}}
             @if($item->hasChildren())
                 @include($view, [ 'items' => $item->getChildren(), 'view' => $view ])
             @endif
-        </li>
+        </c-toc-list-item>
     @endforeach
-</ul>
+</c-toc-list>
